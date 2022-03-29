71bb2e1301b94ceab8c696dc8d95fa181926d8ef
haolinsun <sunhaolin1995@gmail.com>
2022-03-29 22:47:10 +0800

    2022.03.29 leetcode 14 longestCommonPrefix

--- src/main/java/com/example/hello/leetcode/No14longestCommonPrefix.java
+++ src/main/java/com/example/hello/leetcode/No14longestCommonPrefix.java
@@ -1,6 +1,5 @@
 package com.example.hello.leetcode;
 
-import org.apache.logging.log4j.util.Strings;
 
 /**
  * @author 孙浩林
@@ -8,13 +7,26 @@ import org.apache.logging.log4j.util.Strings;
  */
 public class No14longestCommonPrefix {
     public static void main(String[] args) {
-
-
+        //String[] strings = {"flower","flow","flight"};
+        String[] strings = {"a"};
+        String s = longestCommonPrefix(strings);
+        System.out.println(s);
     }
 
     public static String longestCommonPrefix(String[] strings){
-
-
+       if (strings.length==0){
+           return "";
+       }
+       String prefix = strings[0];
+       for (int i = 0; i < strings.length; i++) {
+        while (strings[i].startsWith(prefix)==false){
+            if (prefix.length()==0){
+                return "";
+            }
+            prefix=prefix.substring(0,prefix.length()-1);
+        }
+       }
+       return prefix;
     }
 
 }
