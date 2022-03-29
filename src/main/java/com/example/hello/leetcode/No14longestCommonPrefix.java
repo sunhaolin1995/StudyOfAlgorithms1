676b5d63e1a94552806ab2a21553f00fa88d2875
Haolin.Sun <sunhaolin1995@gmail.com>
2022-03-29 09:48:43 +0800

    2022.03.29update

---
+++ src/main/java/com/example/hello/leetcode/No14longestCommonPrefix.java
@@ -0,0 +1,20 @@
+package com.example.hello.leetcode;
+
+import org.apache.logging.log4j.util.Strings;
+
+/**
+ * @author 孙浩林
+ * @date 2022/3/28 12:54 54
+ */
+public class No14longestCommonPrefix {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static String longestCommonPrefix(String[] strings){
+
+
+    }
+
+}
