980e5b2c4148a7bc5848573d7420168f81958352
haolinsun <sunhaolin1995@gmail.com>
2022-05-26 16:31:24 +0800

    No19RemoveNthFromEnd  ToBeUpdate

---
+++ src/main/java/com/example/hello/leetcode/No19RemoveNthFromEnd.java
@@ -0,0 +1,17 @@
+package com.example.hello.leetcode;
+
+/**
+ * @author 孙浩林
+ * @date: 2022/4/6 12:46 下午
+ */
+public class No19RemoveNthFromEnd {
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n){
+
+
+    }
+
+}
