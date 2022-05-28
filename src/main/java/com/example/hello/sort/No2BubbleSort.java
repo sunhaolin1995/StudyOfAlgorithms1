e1ca28519245e96cf6a4ceb4ebdf585fe95ee71a
MeiHePing <37683119+sunhaolin1995@users.noreply.github.com>
2022-05-28 12:15:20 +0800

    bubbleSort 冒泡排序优化，新增如果一次排序都没有，直接结束

--- src/main/java/com/example/hello/sort/No2BubbleSort.java
+++ src/main/java/com/example/hello/sort/No2BubbleSort.java
@@ -4,6 +4,13 @@ package com.example.hello.sort;
  * @author 孙浩林
  * @date: 2022/5/28 11:07
  */
+
+/**
+ *冒泡排序（Bubble Sort，台湾译为：泡沫排序或气泡排序）是一种简单的排序算法。
+ * 它重复地走访过要排序的数列，一次比较两个元素，如果他们的顺序错误就把他们交换过来。
+ * 走访数列的工作是重复地进行直到没有再需要交换，也就是说该数列已经排序完成。
+ * 这个算法的名字由来是因为越大的元素会经由交换慢慢“浮”到数列的顶端，故名。
+ */
 public class No2BubbleSort {
     public static void main(String[] args) {
         int[] num1 ={2,44,5,67,89,10};
