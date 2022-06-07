3e456863ddf3c99ed02bcf769886c2b98b7de9e4
MeiHePing <37683119+sunhaolin1995@users.noreply.github.com>
2022-06-07 19:57:09 +0800

    insertionSort update!

--- src/main/java/com/example/hello/sort/no1InsertionSort.java
+++ src/main/java/com/example/hello/sort/no1InsertionSort.java
@@ -5,6 +5,8 @@ package com.example.hello.sort;
  * @date: 2022/5/27 16:52
  */
 
+import java.util.Arrays;
+
 /**
  * 插入排序的基本操作就是将一个数据插入到已经排好序的有序数据中，
  * 从而得到一个新的、个数加一的有序数据，算法适用于少量数据的排序，时间复杂度为O(n^2)。
@@ -14,12 +16,19 @@ public class no1InsertionSort {
     public static void main(String[] args) {
        int[] num1 ={2,44,5,67,89,10};
        InsertionSort(num1);
+        System.out.println("排序后的数组："+ Arrays.toString(num1));
     }
 
 
     public static void InsertionSort(int[] nums1){
-        for (int i = 0; i < nums1.length ; i++) {
-            int max =0;
+        for (int i = 1; i < nums1.length ; i++) {
+            int temp = nums1[i];
+            int j = i-1;
+            while (j>=0&&nums1[j]>temp){
+                nums1[j+1]=nums1[j];
+                j--;
+            }
+            nums1[j+1]=temp;
 
         }
     }
