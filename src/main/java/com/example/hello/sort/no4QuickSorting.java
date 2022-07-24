0b53d52671c531317ec5ce887f3c45e95333ad6e
Haolin.Sun <sunhaolin1995@gmail.com>
2022-07-24 21:40:13 +0800

    快速排序待更新

---
+++ src/main/java/com/example/hello/sort/no4QuickSorting.java
@@ -0,0 +1,22 @@
+package com.example.hello.sort;
+
+import java.util.Arrays;
+
+
+public class no4QuickSorting {
+    public static void main(String[] args) {
+        int[] num1 ={2,44,5,67,89,10};
+
+        System.out.println(Arrays.toString(num1));
+    }
+
+    public static void quickSorting(int[] num1,int L,int R){
+       int i=L;
+       int j=R;
+       int pivot=num1[(L+R)/2]
+
+
+    }
+
+
+}
