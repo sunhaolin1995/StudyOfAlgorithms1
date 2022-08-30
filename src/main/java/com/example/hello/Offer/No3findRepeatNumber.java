2bd6444f3b5f004c3f238683fc8dd3e98ea76bfb
haolinsun <2575453225@qq.com>
2022-08-30 23:05:48 +0800

    剑指offer第三题第三种方法，递归

--- src/main/java/com/example/hello/Offer/No3findRepeatNumber.java
+++ src/main/java/com/example/hello/Offer/No3findRepeatNumber.java
@@ -15,6 +15,8 @@ public class No3findRepeatNumber {
 
     }
 
+
+    //第一种方法 hashSet
     /*public static int findRepeatNumber(int[] nums){
         HashSet<Integer> set =new HashSet<>();
         for (int i = 0; i <nums.length ; i++) {
@@ -25,7 +27,8 @@ public class No3findRepeatNumber {
     return  -1;
     }*/
 
-    public static int findRepeatNumber(int[] nums){
+    //第二种方法 遍历数组然后交换
+    /*public static int findRepeatNumber(int[] nums){
 
         for (int i = 0; i<nums.length ; i++) {
             while (nums[i]!=i){
@@ -40,9 +43,41 @@ public class No3findRepeatNumber {
         }
 
         return -1;
+    }*/
+
+    //第三种方法，递归
+    public static int findRepeatNumber(int[] nums){
+      int start =1;
+      int end =nums.length-1;
+      while (end >= start){
+            int middle = ((end - start) / 2)+start;
+            int count=countRange(nums,nums.length,start,middle);
+            if (end==start){
+               if (count>1){
+                   return start;
+               }else {
+                   break;
+               }
+            }
+            if (count >(middle-start)+1){
+                end =middle;
+            }else {
+                start =middle+1;
+            }
+
+      }
+
+        return -1;
     }
 
-    public static void exchangeNum(Integer num ){
+    private static int countRange(int[] nums, int length, int start, int end) {
+         int count =0;
+        for (int i = 0; i <length ; i++) {
+            if (nums[i]>=start && nums[i]<=end ){
+                ++count;
+            }
+        }
+        return count;
 
     }
 
