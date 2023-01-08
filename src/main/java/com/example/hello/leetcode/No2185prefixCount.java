88451513ce3b9734fd02f843191f54974fd018c9
Haolin.Sun <sunhaolin1995@gmail.com>
2023-01-08 13:54:19 +0800

    2023/01/08 13:53:56 解答成功: 	执行耗时:4 ms,击败了14.10% 的Java用户 	内存消耗:41.6 MB,击败了34.46% 的Java用户

---
+++ src/main/java/com/example/hello/leetcode/No2185prefixCount.java
@@ -0,0 +1,36 @@
+package com.example.hello.leetcode;
+
+public class No2185prefixCount {
+
+    public static void main(String[] args) {
+        String[] words = {"leetcode","win","loops","success"};
+        String pref = "code";
+        System.out.println(prefixCount(words,pref));
+    }
+
+    public static int prefixCount(String[] words, String pref) {
+        int result = 0;
+
+        for (int i = 0; i < words.length ; i++) {
+            if (pref.length() <= words[i].length()){
+                int a =0 ,b=0;
+                while (b < pref.length()){
+                    if (pref.charAt(a) ==  words[i].charAt(a) ){
+                        a++;
+                        b++;
+                    }else {
+                        b++;
+                    }
+                }
+                if (a == pref.length()){
+                    result++;
+                }
+
+            }
+
+        }
+        return result;
+    }
+
+
+}
