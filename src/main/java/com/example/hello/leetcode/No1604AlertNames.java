9dabcf5bf3d804c793a1fe97b109a9aa49d2f344
Haolin.Sun <sunhaolin1995@gmail.com>
2023-02-07 20:57:38 +0800

    执行用时： 116 ms , 在所有 Java 提交中击败了 31.25% 的用户 内存消耗： 62.2 MB , 在所有 Java 提交中击败了 40.00% 的用户 通过测试用例： 77 / 77

--- src/main/java/com/example/hello/leetcode/No1604AlertNames.java
+++ src/main/java/com/example/hello/leetcode/No1604AlertNames.java
@@ -1,7 +1,8 @@
 package com.example.hello.leetcode;
 
-import java.util.HashMap;
-import java.util.List;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * @author 孙浩林
@@ -22,9 +23,35 @@ public class No1604AlertNames {
     }
 
     public static List<String> alertNames(String[] keyName, String[] keyTime) {
-        HashMap<String,String> key = new HashMap<>();
-        
+        Map<String,List<Integer>> convertMap = new HashMap<>();
 
+        for (int i = 0; i < keyName.length ; i++) {
+            String name = keyName[i];
+            String time = keyTime[i];
+            convertMap.putIfAbsent(name,new ArrayList<>());
+
+
+            int timeInMinute =Integer.valueOf(time.split(":")[0])*60 + Integer.valueOf(time.split(":")[1]);
+            List<Integer> integers = convertMap.get(name);
+            integers.add(timeInMinute);
+            convertMap.put(name,integers);
+        }
+
+        List<String> result = new ArrayList<>();
+        Set<String> strings = convertMap.keySet();
+        for (String key :strings) {
+            List<Integer> times = convertMap.get(key);
+            Collections.sort(times);
+            for (int i = 2; i < times.size() ; i++) {
+                if (times.get(i) - times.get(i-2)<=60 ){
+                    result.add(key);
+                    break;
+                }
+            }
+        }
+
+        Collections.sort(result);
+        return result;
     }
 
 }
