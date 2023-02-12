318a380401574922fc6894c9e88ae11925d8e80f
Haolin.Sun <sunhaolin1995@gmail.com>
2023-02-12 22:06:30 +0800

    完成了思路，代码是cv的

---
+++ src/main/java/com/example/hello/leetcode/No1138AlphabetBoardPath.java
@@ -0,0 +1,53 @@
+package com.example.hello.leetcode;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class No1138AlphabetBoardPath {
+
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public String alphabetBoardPath(String target) {
+
+        int cx = 0, cy = 0;
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < target.length(); i++) {
+            char c = target.charAt(i);
+            int nx = (c - 'a') / 5;
+            int ny = (c - 'a') % 5;
+            if (nx < cx) {
+                for (int j = 0; j < cx - nx; j++) {
+                    res.append('U');
+                }
+            }
+            if (ny < cy) {
+                for (int j = 0; j < cy - ny; j++) {
+                    res.append('L');
+                }
+            }
+            if (nx > cx) {
+                for (int j = 0; j < nx - cx; j++) {
+                    res.append('D');
+                }
+            }
+            if (ny > cy) {
+                for (int j = 0; j < ny - cy; j++) {
+                    res.append('R');
+                }
+            }
+            res.append('!');
+            cx = nx;
+            cy = ny;
+        }
+        return res.toString();
+
+    }
+
+}
