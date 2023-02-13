13dda9f952f2520ed42e7bad7f33a2304c557fab
Haolin.Sun <sunhaolin1995@gmail.com>
2023-02-13 21:06:46 +0800

    执行用时: 6 ms 内存消耗: 41.8 MB

--- src/main/java/com/example/hello/leetcode/No1234BalanceString.java
+++ src/main/java/com/example/hello/leetcode/No1234BalanceString.java
@@ -15,7 +15,9 @@ public class No1234BalanceString {
         System.out.println(balancedString(s));
     }
 
-    public static int balancedString(String s) {
+
+    //我认为我的做法是对的，但是题解要求用滑动窗口做，所以没办法
+   /* public static int balancedString(String s) {
         int[] nums = {0,0,0,0};
 
         for (int i = 0; i <s.length() ; i++) {
@@ -42,9 +44,62 @@ public class No1234BalanceString {
             result++;
             Arrays.sort(nums);
         }
-
         return result;
+    }*/
+
+    public static int balancedString(String s){
+        int[] counts = new int[26];
+        int len = s.length();
+        int limit = len/4;
+
+        char ch;
+
+        //初始化不替換内容字符出現次數數組，即滑動窗口維護一個空串
+        for (int i = 0; i < len; i++) {
+            ch = s.charAt(i);
+            counts[ch - 'A']++;
+        }
+        // 初始化滑动窗口左右指针，维护的子串是[left,right]的内容
+        // 初始化子串为空，因此left=0，right=-1表示一个空子串
+        int left = 0;
+        int right =-1;
+        int minLength = len;//
+
+        while (left < len){
+            //校驗通過
+            //記錄當前合法子串長度并更新最小長度
+            if (check(counts,limit)){
+               //左指針右移
+                minLength = Math.min(minLength,right-left+1);
+                counts[s.charAt(left++) - 'A']++;
+            } else if (right < len -1 ) {
+                counts[s.charAt(++right) - 'A'] --;
+            }else {
+                break;
+            }
+        }
+
+
+
+     return minLength;
+
+
+
 
     }
+
+    /**
+     * 校验函数，校验当前counts中四个字符的出现次数是否都小于等于limit；
+     * 是返回true，否返回false
+     */
+    private static boolean check(int[] counts, int limit){
+        if(counts['Q' - 'A'] > limit || counts['W' - 'A'] > limit || counts['E' - 'A'] > limit || counts['R' - 'A'] > limit){
+            return false;
+        }
+        return true;
+    }
+
+
+
 
 }
