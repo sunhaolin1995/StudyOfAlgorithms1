7cc916137719c36b6d7a1e7d1975f5e7e61e8b47
Haolin.Sun <sunhaolin1995@gmail.com>
2023-03-01 23:57:03 +0800

    测试

---
+++ src/main/java/com/example/hello/leetcode/No2373LargestLocal.java
@@ -0,0 +1,25 @@
+package com.example.hello.leetcode;
+
+public class No2373LargestLocal {
+    public static void main(String[] args) {
+        int[][] grid = {{9,9,8,1},{5,6,2,6},{8,2,6,4},{6,2,2,2}};
+
+
+    }
+
+    public static int[][] largestLocal(int[][] grid) {
+        int n = grid.length;
+        int[][] res = new int[n - 2][n - 2];
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                for (int x = i; x < i + 3; x++) {
+                    for (int y = j; y < j + 3; y++) {
+                        res[i][j] = Math.max(res[i][j], grid[x][y]);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+}
