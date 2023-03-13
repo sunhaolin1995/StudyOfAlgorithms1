6485ec8a779d7f7f6f8ccec969813024c6974bfc
Haolin.Sun <sunhaolin1995@gmail.com>
2023-03-13 22:45:51 +0800

    fix

--- src/main/java/com/example/hello/leetcode/No66plusOne.java
+++ src/main/java/com/example/hello/leetcode/No66plusOne.java
@@ -15,16 +15,16 @@ public class No66plusOne {
     }
 
     public static int[] plusOne(int[] digits){
-        for (int i = digits.length-1; i >=0 ; i--) {
-           if (digits[i]!=9){
-               digits[i]++;
-               return digits;
-           }
-          digits[i]=0;
+        for (int i = digits.length -1;i >=0 ; i--) {
+            if (digits[i] != 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
         }
-     int[] newDigits=new int[digits.length+1];
-     newDigits[0]=1;
-     return newDigits;
+        int[] newres = new int[digits.length+1];
+        newres[0]=1;
+        return newres;
 
     }
 }
