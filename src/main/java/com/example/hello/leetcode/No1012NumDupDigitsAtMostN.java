9d90848aa9e592ec4649e3450cdd4f02c5eb56c3
haolinsun <2575453225@qq.com>
2023-03-20 17:26:06 +0800

    to be continued

---
+++ src/main/java/com/example/hello/leetcode/No1012NumDupDigitsAtMostN.java
@@ -0,0 +1,28 @@
+package com.example.hello.leetcode;
+
+/**
+ * @author 孙浩林
+ * @date: 3/20/23 17:25
+ */
+public class No1012NumDupDigitsAtMostN {
+    public static void main(String[] args) {
+
+    }
+
+    public static int numDupDigitsAtMostN(int n) {
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (checkIsDup(n));
+        }
+        return res;
+    }
+    public static boolean checkIsDup(int n){
+        Character[] convert = new Character[n];
+        for (int i = 0; i <n ; i++) {
+            convert[i] =
+
+
+        }
+
+    }
+}
