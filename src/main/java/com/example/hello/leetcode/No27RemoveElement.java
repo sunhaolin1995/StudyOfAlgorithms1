9946ca46b1b56c1a214befa1a20fd682b75abb0d
haolinsun <2575453225@qq.com>
2023-09-06 17:43:39 +0800

    to be update

--- src/main/java/com/example/hello/leetcode/No27RemoveElement.java
+++ src/main/java/com/example/hello/leetcode/No27RemoveElement.java
@@ -8,23 +8,52 @@ public class No27RemoveElement {
 
 
     public static void main(String[] args) {
-        int[] num ={0,1,2,2,3,0,4,2};
-        int i = removeElement(num, 2);
+        int[] num = {3,2,2,3};
+        int i = removeElement(num, 3);
         System.out.println(i);
     }
 
     public static int removeElement(int[] nums, int val) {
-        int a=0;
+        int leftNum = nums.length-2, rightNum = nums.length-1;
+        while (leftNum >= 0){
+            if (nums[rightNum] == val){
+                //exchangeValue
+                int temp = nums[leftNum];
+                nums[leftNum] =nums[rightNum];
+                nums[rightNum] =temp;
+                rightNum--;
+            }
+            leftNum--;
+        }
+        return rightNum;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*int a=0;
         for (int i = 0; i <nums.length ; i++) {
             if (nums[i]!=val){
                 nums[a]= nums[i];
                 a++;
             }
         }
+        return a;*/
         /*while (a< nums.length){
             nums[a++]=0;
         }*/
-        return a;
        /* if (nums == null || nums.length == 0)
             return 0;
         int j = 0;
