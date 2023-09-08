47827516ed4f08a1da2e4105882deccdf2203222
MeiHePing <sunhaolin1995@gmail.com>
2023-09-08 23:35:49 +0800

    update

--- src/main/java/com/example/hello/leetcode/No189Rotate.java
+++ src/main/java/com/example/hello/leetcode/No189Rotate.java
@@ -6,14 +6,57 @@ package com.example.hello.leetcode;
  */
 public class No189Rotate {
     public static void main(String[] args) {
-      int[] nums ={1,2,3,4,5,6,7};
-      int k=3;
+      int[] nums ={-1};
+      int k=2;
 
       rotate(nums,k);
       for (int i = 0; i < nums.length; i++) {
           System.out.println(nums[i]);
       }
     }
+
+    public static void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+        int[] newArr = new int[n];
+
+        for (int i = 0; i < n ; i++) {
+            newArr[(i+k)%n] = nums[i];
+        }
+        for (int i = 0; i <n ; i++) {
+            nums[i] = newArr[i];
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);*/
+    }
+
 
     private static void reverse(int[] nums, int start, int end) {
         for (int i = start, j = end; i < j; i++, j--) {
@@ -23,13 +66,7 @@ public class No189Rotate {
         }
     }
 
-    public static void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k %= n;
-        reverse(nums, 0, n - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
-    }
+
 
 
 }
