d7118d772f09cc6b3b2660c0ff4adeea5a80bb28
MeiHePing <sunhaolin1995@gmail.com>
2023-09-10 17:26:03 +0800

    122maxProfit complete

--- src/main/java/com/example/hello/leetcode/No122maxProfit.java
+++ src/main/java/com/example/hello/leetcode/No122maxProfit.java
@@ -6,18 +6,45 @@ package com.example.hello.leetcode;
  */
 public class No122maxProfit {
     public static void main(String[] args) {
-        int[] prices = {7,1,5,3,6,4};
+        int[] prices = {1,2,3,4,5};
         System.out.println(maxProfit(prices));
     }
 
     public static int maxProfit(int[] prices){
-        int price =0;
+        int result = 0;
+
+        for (int i = 0; i < prices.length -1 ; i++) {
+            if (prices[i] <= prices[i+1]){
+                result = result+(prices[i+1]-prices[i]);
+            }
+        }
+        return  result;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       /* int price =0;
         for (int i = 0; i <prices.length-1 ; i++) {
           if (prices[i]<prices[i+1]){
               price+=prices[i+1]-prices[i];
           }
         }
-        return price;
+        return price;*/
     }
 
 }
