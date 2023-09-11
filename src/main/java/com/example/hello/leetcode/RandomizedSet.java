0ec184422442c3999743d2a1763d1abc4c8989a1
haolinsun <2575453225@qq.com>
2023-09-11 17:17:47 +0800

    O(1) insert remove and random.that is awesome

---
+++ src/main/java/com/example/hello/leetcode/RandomizedSet.java
@@ -0,0 +1,54 @@
+package com.example.hello.leetcode;
+
+import com.oracle.tools.packager.mac.MacAppBundler;
+
+import java.util.*;
+
+/**
+ * @author 孙浩林
+ * @date: 9/11/23 16:41
+ */
+public class RandomizedSet {
+
+    private List<Integer> list;
+
+    private Map<Integer,Integer> map;
+
+    private Random random;
+
+    public RandomizedSet() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        random = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)){
+            return false;
+        }
+        int index =list.size();
+        list.add(val);
+        map.put(val,index);
+
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)){
+            return false;
+        }
+        //将要删除的元素和最后一个元素交换位置，然后删除最后一个元素
+        int indexToRemove = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+        list.set(indexToRemove,lastElement);
+        map.put(lastElement,indexToRemove);
+        list.remove(list.size()-1);
+        map.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        int randomIndex = random.nextInt(list.size());
+        return list.get(randomIndex);
+    }
+}
