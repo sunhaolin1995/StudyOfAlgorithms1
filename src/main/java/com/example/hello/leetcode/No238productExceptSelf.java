084f8c1d0075cf52ee16b7fa8a7f9663a7961ba4
haolinsun <2575453225@qq.com>
2023-09-11 17:35:25 +0800

    to be update

---
+++ src/main/java/com/example/hello/leetcode/No238productExceptSelf.java
@@ -0,0 +1,21 @@
+package com.example.hello.leetcode;
+
+
+public class No238productExceptSelf {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        int[] ints = productExceptSelf(nums);
+        for (int i : ints
+        ) {
+            System.out.println(i);
+        }
+
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+
+    }
+
+
+}
