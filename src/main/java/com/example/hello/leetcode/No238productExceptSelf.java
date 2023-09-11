63830de63595a6121253483604a83754e0ee89f3
MeiHePing <sunhaolin1995@gmail.com>
2023-09-11 22:43:56 +0800

    compelte No238

--- src/main/java/com/example/hello/leetcode/No238productExceptSelf.java
+++ src/main/java/com/example/hello/leetcode/No238productExceptSelf.java
@@ -14,7 +14,23 @@ public class No238productExceptSelf {
     }
 
     public static int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] res = new int[length];
+        int[] leftNum = new int[length];
+        leftNum[0] = 1;
+        for (int i = 1; i < length; i++) {
+            leftNum[i] = leftNum[i-1]*nums[i-1];
+        }
 
+        int[] rightNum = new int[length];
+        rightNum[length-1] = 1;
+        for (int i = length-2; i >=0 ; i--) {
+            rightNum[i] = rightNum[i+1]*nums[i+1];
+        }
+        for (int i = 0; i < length; i++) {
+            res[i] = leftNum[i]*rightNum[i];
+        }
+        return res;
     }
 
 
