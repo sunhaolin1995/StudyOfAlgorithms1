7b3f1484500cdfb255c0f4837e0b8170e671beff
haolinsun <2575453225@qq.com>
2023-09-20 16:30:18 +0800

    complete 14 by chatGPT

--- src/main/java/com/example/hello/leetcode/No14longestCommonPrefix.java
+++ src/main/java/com/example/hello/leetcode/No14longestCommonPrefix.java
@@ -7,26 +7,57 @@ package com.example.hello.leetcode;
  */
 public class No14longestCommonPrefix {
     public static void main(String[] args) {
-        //String[] strings = {"flower","flow","flight"};
-        String[] strings = {"a"};
+        String[] strings = {"flower","flow","flight"};
+        //String[] strings = {"a"};
         String s = longestCommonPrefix(strings);
         System.out.println(s);
     }
 
-    public static String longestCommonPrefix(String[] strings){
-       if (strings.length==0){
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String compare = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            char[] charArray = compare.toCharArray();
+            int length = charArray.length;
+            char[] charArrayStrings = strs[i].toCharArray();
+            int length1 = charArrayStrings.length;
+            compare = "";
+            for (int j = 0; j < Math.min(length, length1); j++) {
+                if (charArray[j] == charArrayStrings[j]) {
+                    compare = compare + charArray[j];
+                } else {
+                    break;
+                }
+            }
+        }
+        return compare;
+
+
+
+
+
+
+
+
+
+
+
+        /*
+       if (strs.length==0){
            return "";
        }
-       String prefix = strings[0];
-       for (int i = 0; i < strings.length; i++) {
-        while (strings[i].startsWith(prefix)==false){
+       String prefix = strs[0];
+       for (int i = 0; i < strs.length; i++) {
+        while (strs[i].startsWith(prefix)==false){
             if (prefix.length()==0){
                 return "";
             }
             prefix=prefix.substring(0,prefix.length()-1);
         }
        }
-       return prefix;
+       return prefix;*/
     }
 
 }
