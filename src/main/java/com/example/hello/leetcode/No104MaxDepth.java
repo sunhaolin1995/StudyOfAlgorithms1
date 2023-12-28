43103cc3ca73927c2028e20eb9517b16b71682df
haolinsun <2575453225@qq.com>
2023-12-28 11:09:08 +0800

    complete with recursion

---
+++ src/main/java/com/example/hello/leetcode/No104MaxDepth.java
@@ -0,0 +1,31 @@
+package com.example.hello.leetcode;
+
+
+public class No104MaxDepth {
+
+    public static void main(String[] args) {
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode9 = new TreeNode(9);
+        TreeNode treeNode20 = new TreeNode(20);
+        TreeNode treeNode15 = new TreeNode(15);
+        TreeNode treeNode7 = new TreeNode(7);
+
+        treeNode3.left = treeNode9;
+        treeNode3.right = treeNode20;
+        treeNode20.left = treeNode15;
+        treeNode20.right = treeNode7;
+        System.out.println(maxDepth(treeNode3));
+
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftMaxDepth = maxDepth(root.left);
+        int rightMaxDepth = maxDepth(root.right);
+        return Math.max(leftMaxDepth, rightMaxDepth) + 1;
+    }
+
+}
+
