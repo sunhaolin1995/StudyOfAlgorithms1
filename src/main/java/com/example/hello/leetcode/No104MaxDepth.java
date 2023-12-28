40c21fddb3d332e805a78fda2390ec6861d0b4d3
haolinsun <2575453225@qq.com>
2023-12-28 13:28:35 +0800

    complete with breadth first search

--- src/main/java/com/example/hello/leetcode/No104MaxDepth.java
+++ src/main/java/com/example/hello/leetcode/No104MaxDepth.java
@@ -1,6 +1,9 @@
 package com.example.hello.leetcode;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class No104MaxDepth {
 
     public static void main(String[] args) {
@@ -19,13 +22,42 @@ public class No104MaxDepth {
     }
 
     public static int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int ans =0;
+
+        while ( !queue.isEmpty()){
+            int size = queue.size();
+            while (size >0){
+                TreeNode node = queue.poll();
+                if (node.left != null){
+                    queue.offer(node.left);
+                }
+                if (node.right != null){
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+
+    }
+
+
+    /*public static int maxDepth(TreeNode root) {
+    // depth first search
         if (root == null) {
             return 0;
         }
         int leftMaxDepth = maxDepth(root.left);
         int rightMaxDepth = maxDepth(root.right);
         return Math.max(leftMaxDepth, rightMaxDepth) + 1;
-    }
+    }*/
 
 }
 
