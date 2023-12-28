160d869ceabb89e9eb7d876ce4b346edeea7919a
haolinsun <2575453225@qq.com>
2023-12-28 16:03:01 +0800

    complete by BFS(breadth first search)

--- src/main/java/com/example/hello/leetcode/No100IsSameTree.java
+++ src/main/java/com/example/hello/leetcode/No100IsSameTree.java
@@ -1,5 +1,8 @@
 package com.example.hello.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author 孙浩林
  * @date: 12/28/23 13:29
@@ -20,6 +23,51 @@ public class No100IsSameTree {
     }
 
     public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        }
+        Queue<TreeNode> queue1 = new LinkedList<TreeNode>();
+        Queue<TreeNode> queue2 = new LinkedList<TreeNode>();
+        queue1.offer(p);
+        queue2.offer(q);
+
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            TreeNode node1 = queue1.poll();
+            TreeNode node2 = queue2.poll();
+            if (node1.val != node2.val) {
+                return false;
+            }
+            TreeNode left1 = node1.left;
+            TreeNode right1 = node1.right;
+            TreeNode left2 = node2.left;
+            TreeNode right2 = node2.right;
+
+            if (left1 == null ^ left2 == null) {
+                return false;
+            }
+            if (right1 == null ^ right2 == null) {
+                return false;
+            }
+            if (left1 != null) {
+                queue1.offer(left1);
+            }
+            if (right1 != null) {
+                queue1.offer(right1);
+            }
+            if (left2 != null) {
+                queue2.offer(left2);
+            }
+            if (right2 != null) {
+                queue2.offer(right2);
+            }
+
+        }
+        return queue1.isEmpty() && queue2.isEmpty();
+    }
+
+    /*public static boolean isSameTree(TreeNode p, TreeNode q) {
 
         if (p == null && q == null) {
             return true;
@@ -34,6 +82,6 @@ public class No100IsSameTree {
             return false;
         }
 
-    }
+    }*/
 
 }
