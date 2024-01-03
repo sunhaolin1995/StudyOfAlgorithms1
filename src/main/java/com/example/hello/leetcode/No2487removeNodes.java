60d9d5c9ff93db17baba724e9387a31c5fd1244f
MeiHePing <sunhaolin1995@gmail.com>
2024-01-03 14:24:28 +0800

    complete with stack(use ArrayDeque)

--- src/main/java/com/example/hello/leetcode/No2487removeNodes.java
+++ src/main/java/com/example/hello/leetcode/No2487removeNodes.java
@@ -1,12 +1,16 @@
 package com.example.hello.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
 public class No2487removeNodes {
     public static void main(String[] args) {
-        ListNode listNode1 = new ListNode(1);
-        ListNode listNode2 = new ListNode(1);
-        ListNode listNode3 = new ListNode(1);
-        ListNode listNode4 = new ListNode(1);
-        ListNode listNode5 = new ListNode(1);
+        ListNode listNode1 = new ListNode(5);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(13);
+        ListNode listNode4 = new ListNode(3);
+        ListNode listNode5 = new ListNode(8);
         listNode1.next = listNode2;
         listNode2.next = listNode3;
         listNode3.next = listNode4;
@@ -19,6 +23,27 @@ public class No2487removeNodes {
     }
 
     public static ListNode removeNodes(ListNode head) {
+        Deque<ListNode> stack = new ArrayDeque<>();
+        while (head != null){
+            stack.push(head);
+            head = head.next;
+        }
+
+        /*ListNode dummy = new ListNode(-1);
+        ListNode res = dummy;*/
+        while(!stack.isEmpty()){
+            if (head==null || stack.peek().val >= head.val){
+                stack.peek().next =head;
+                head =stack.peek();
+            }
+            stack.pop();
+        }
+
+        return head;
+    }
+
+
+    /*public static ListNode removeNodes(ListNode head) {
         if (head == null){
             return null;
         }
@@ -29,5 +54,5 @@ public class No2487removeNodes {
             return head;
         }
 
-    }
+    }*/
 }
