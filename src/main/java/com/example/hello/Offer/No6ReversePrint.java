3731219f464c9772b08269809453ba8cef7b9727
haolinsun <2575453225@qq.com>
2024-01-16 09:31:37 +0800

    use stack to store the data

--- src/main/java/com/example/hello/Offer/No6ReversePrint.java
+++ src/main/java/com/example/hello/Offer/No6ReversePrint.java
@@ -1,6 +1,8 @@
 package com.example.hello.Offer;
 
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 /**
@@ -16,13 +18,29 @@ public class No6ReversePrint {
         ListNode numThree = new ListNode(2);
         numTwo.next = numThree;
 
-
         int[] ints = reversePrint(head);
         for (int i = 0; i < ints.length; i++) {
             System.out.println(ints[i]);
         }
 
     }
+
+    //use stack to store the data
+    public static int[] reversePrint(ListNode head) {
+        Deque<Integer> cvt = new ArrayDeque<>();
+        while (head != null){
+            cvt.push(head.val);
+            head = head.next;
+        }
+        int[] res =new int[cvt.size()];
+        int i =0;
+        while (!cvt.isEmpty()){
+            res[i] = cvt.pop();
+            i++;
+        }
+        return res;
+    }
+
 
     /*public int[] reversePrint(ListNode head) {
         ListNode node = head;
@@ -44,25 +62,22 @@ public class No6ReversePrint {
 
     }*/
 
-    public static int[] reversePrint(ListNode head) {
+    /*public static int[] reversePrint(ListNode head) {
         Stack<Integer> reversePrint = new Stack();
         while(head != null){
             reversePrint.push(head.val);
             head = head.next;
         }
-
         int[] result = new int[reversePrint.size()];
-
         for (int i = 0; i < result.length; i++) {
-            /***
+            *//***
              * The only difference between peek and pop is that the peek method just returns the topmost element;
              * however, in the case of a pop method,the topmost element is returned and also that element is deleted from the stack.
-             */
+             *//*
             result[i] = reversePrint.pop();
 
         }
-
         return result;
-    }
+    }*/
 
 }
