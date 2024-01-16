382fd5552339566ec658cdf153afaa9fb6bba866
haolinsun <2575453225@qq.com>
2024-01-16 09:14:10 +0800

    timeComplexity is O(n)

--- src/main/java/com/example/hello/Offer/No5ReplaceSpace.java
+++ src/main/java/com/example/hello/Offer/No5ReplaceSpace.java
@@ -7,12 +7,41 @@ package com.example.hello.Offer;
 public class No5ReplaceSpace {
 
     public static void main(String[] args) {
-        String s = "We are happy." ;
+        String s = "We are happy.";
         System.out.println(replaceSpace(s));
     }
 
     public static String replaceSpace(String s) {
-        return s.replace(" ","%20");
+
+        int blankNum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)==' '){
+                blankNum++;
+            }
+        }
+        char[] newString = new char[s.length() + 2 * blankNum];
+
+        int pre = s.length() - 1, next = s.length() + 2 * blankNum - 1;
+        char[] oldCharArray = s.toCharArray();
+        while (pre >= 0) {
+            if (oldCharArray[pre] != ' ') {
+                newString[next] = oldCharArray[pre];
+                next--;
+                pre--;
+            } else {
+                newString[next] ='0';
+                newString[next-1]='2';
+                newString[next-2]='%';
+                pre--;
+                next=next-3;
+            }
+
+        }
+        return new String(newString);
     }
+
+    /*public static String replaceSpace(String s) {
+        return s.replace(" ","%20");
+    }*/
 
 }
