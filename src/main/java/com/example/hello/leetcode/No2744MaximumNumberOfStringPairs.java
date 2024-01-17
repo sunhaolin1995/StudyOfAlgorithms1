8f7b28fe9190c513c645569a987059aecc0a73b2
haolinsun <2575453225@qq.com>
2024-01-17 10:01:43 +0800

    simulate

---
+++ src/main/java/com/example/hello/leetcode/No2744MaximumNumberOfStringPairs.java
@@ -0,0 +1,35 @@
+package com.example.hello.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 孙浩林
+ * @date: 1/17/24 08:44
+ */
+public class No2744MaximumNumberOfStringPairs {
+    public static void main(String[] args) {
+        String[] words = {"ff","tx","qr","zw","wr","jr","zt","jk","sq","xx"};
+        System.out.println(maximumNumberOfStringPairs(words));
+
+    }
+
+    public static int maximumNumberOfStringPairs(String[] words) {
+        Map<String, Integer> cvt = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+                cvt.put(words[i], cvt.getOrDefault(words[i],0)+1);
+        }
+        int res =0;
+        for (String key: cvt.keySet()) {
+            String newKey ="";
+            newKey=newKey+key.charAt(1)+key.charAt(0);
+            if (key.charAt(0)==key.charAt(1)){
+                res=res+ (cvt.get(key)/2)*2;
+            } else if (cvt.containsKey(newKey)){
+                res =res+ Math.min(cvt.get(key),cvt.get(newKey));
+            }
+        }
+        return res/2;
+    }
+
+}
