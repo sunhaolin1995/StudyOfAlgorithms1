39e1a134b9ff93ea5e9fb368f5785f219e487541
haolinsun <2575453225@qq.com>
2024-01-17 14:09:23 +0800

    binary search

--- src/main/java/com/example/hello/Offer/No11minArray.java
+++ src/main/java/com/example/hello/Offer/No11minArray.java
@@ -6,19 +6,44 @@ package com.example.hello.Offer;
  */
 public class No11minArray {
     public static void main(String[] args) {
-
+        int[] numbers = {1, 0, 1, 1, 1};
+        System.out.println(minArray(numbers));
     }
 
-    public static int minArray(int[] numbers){
-        int i =0,num = numbers[0],temp = 5000;
-        while (i < numbers.length){
-            if (numbers[i] < temp){
-                num = numbers[i];
-                temp = numbers[i];
+    public static int minArray(int[] numbers) {
+        int index1 = 0, index2 = numbers.length - 1;
+        int indexMid = index1;
+
+        while (numbers[index1] >= numbers[index2]) {
+            if (index2 - index1 == 1) {
+                indexMid = index2;
+                break;
             }
-            i++;
+
+            indexMid = (index1 + index2) / 2;
+
+            if (numbers[index1] == numbers[index2] && numbers[index1] == numbers[indexMid]){
+                return MinInOrder(numbers,index1,index2);
+            }
+
+            if (numbers[indexMid] >= numbers[index1]) {
+                index1 = indexMid;
+            } else if (numbers[indexMid] <= numbers[index2]) {
+                index2 = indexMid;
+            }
+
         }
-        return num;
+        return numbers[indexMid];
+    }
+
+    public static int MinInOrder(int[] numbers,int index1,int index2){
+        int res = numbers[index1];
+        for (int i = index1 +1 ; i <=index2 ; i++) {
+            if (res > numbers[i]){
+                res = numbers[i];
+            }
+        }
+        return res;
     }
 
 }
