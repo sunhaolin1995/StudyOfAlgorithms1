f0619b34b101b31478a86e469fd501eda3b7a7af
haolinsun <2575453225@qq.com>
2024-01-17 08:43:16 +0800

    no 10 fibonacci ,Use loop replace recursion

--- src/main/java/com/example/hello/Offer/No10fib.java
+++ src/main/java/com/example/hello/Offer/No10fib.java
@@ -6,23 +6,28 @@ package com.example.hello.Offer;
  */
 public class No10fib {
     public static void main(String[] args) {
-        System.out.println(fib(3));
+        System.out.println(fib(100));
     }
 
 
-    public static int fib(Integer n){
-        if (n <=0){
+    public static int fib(Integer n) {
+        if (n <= 0) {
             return 0;
         }
-        if (n == 1){
+        if (n == 1) {
             return 1;
         }
-        return fib(n-1)+fib(n-2);
+        int p = 1, q = 0, res = 1;
+
+        for (int i = 0; i < n - 2; i++) {
+            q =p;
+            p =res;
+            res = p+q;
+            //res = (p+q)%1000000007; //prevent stack over flow
+        }
+        return res;
 
     }
-
-
-
 
 
 }
