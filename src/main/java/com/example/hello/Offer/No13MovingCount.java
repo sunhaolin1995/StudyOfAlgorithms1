4888940e732f4ee8d7c7938ecfa1ffe12df11b21
haolinsun <2575453225@qq.com>
2024-01-21 13:28:10 +0800

    add a Two-dimensional array to store whether the place is been marked

--- src/main/java/com/example/hello/Offer/No13MovingCount.java
+++ src/main/java/com/example/hello/Offer/No13MovingCount.java
@@ -1,48 +1,48 @@
 package com.example.hello.Offer;
 
 public class No13MovingCount {
-
     public static void main(String[] args) {
-        int threshold = 1, rows = 2, cols = 3;
+        int threshold = 10, rows = 1, cols = 100;
         System.out.println(movingCount(threshold, rows, cols));
     }
 
+
     public static int count = 0;
 
+
     public static int movingCount(int threshold, int rows, int cols) {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                dfs(threshold, i, j, rows, cols);
-            }
-        }
+        boolean[][] cvt = new boolean[rows][cols];
+
+        dfs(threshold, 0, 0, rows, cols, cvt);
+
         return count;
     }
 
-    public static void dfs(int threshold, int i, int j, int rows, int cols) {
+    public static void dfs(int threshold, int i, int j, int rows, int cols, boolean[][] cvt) {
 
-        if (i < 0 || j < 0 || i > rows || j > cols) {
+        if (i < 0 || j < 0 || i >= rows || j >= cols || cvt[i][j]) {
             return;
         }
         int sum = 0;
-        while (i > 0) {
-            sum += i % 10;
-            i /= 10;
+        int ti = i, tj = j;
+        while (ti > 0) {
+            sum += ti % 10;
+            ti /= 10;
         }
-        while (j > 0) {
-            sum += j % 10;
-            j /= 10;
+        while (tj > 0) {
+            sum += tj % 10;
+            tj /= 10;
         }
-
-        if (sum > threshold) {
-            return;
-        }else {
+        if (sum <= threshold) {
             count++;
+        } else {
+            return;
         }
-
-        dfs(threshold,i+1,j,rows,cols);
-        dfs(threshold,i,j+1,rows,cols);
-        dfs(threshold,i-1,j,rows,cols);
-        dfs(threshold,i,j-1,rows,cols);
+        cvt[i][j] = true;
+        dfs(threshold, i + 1, j, rows, cols, cvt);
+        dfs(threshold, i, j + 1, rows, cols, cvt);
+        dfs(threshold, i - 1, j, rows, cols, cvt);
+        dfs(threshold, i, j - 1, rows, cols, cvt);
     }
 
 }
