cc2fe6b10b9efa3021edaf59fb00aaef2d88a501
haolinsun <2575453225@qq.com>
2024-01-23 14:18:29 +0800

    dfs

---
+++ src/main/java/com/example/hello/Offer/No27Mirror.java
@@ -0,0 +1,43 @@
+package com.example.hello.Offer;
+
+/**
+ * @author 孙浩林
+ * @date: 1/23/24 13:54
+ */
+public class No27Mirror {
+
+    public static void main(String[] args) {
+// 树：[8,6,10,5,7,9,11]
+        TreeNode root = new TreeNode(8);
+        root.left = new TreeNode(6);
+        root.right = new TreeNode(10);
+        root.left.left = new TreeNode(5);
+        root.left.right = new TreeNode(7);
+        root.right.left = new TreeNode(9);
+        root.right.right = new TreeNode(11);
+        TreeNode mirror = Mirror(root);
+
+        printTree(mirror);
+
+    }
+
+    public static void printTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.println(root.val);
+        printTree(root.left);
+        printTree(root.right);
+    }
+
+    public static TreeNode Mirror(TreeNode pRoot) {
+        if (pRoot == null) {
+            return null;
+        }
+        TreeNode res = new TreeNode(pRoot.val);
+        res.left = Mirror(pRoot.right);
+        res.right = Mirror(pRoot.left);
+        return res;
+    }
+
+}
