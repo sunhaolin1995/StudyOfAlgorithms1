91434965970cdd443203b29f7078274e6dab2949
MeiHePing <sunhaolin1995@gmail.com>
2024-02-18 12:33:40 +0800

    partition solution

--- src/main/java/com/example/hello/Offer/No40getLeastNumbers.java
+++ src/main/java/com/example/hello/Offer/No40getLeastNumbers.java
@@ -1,8 +1,6 @@
 package com.example.hello.Offer;
 
 import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class No40getLeastNumbers {
 
@@ -12,7 +10,50 @@ public class No40getLeastNumbers {
         System.out.println(integers.toString());
     }
 
+
     public static ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (input == null || k <= 0 || k > input.length) {
+            return result;
+        }
+        quickSort(input, 0, input.length - 1);
+        for (int i = 0; i < k; i++) {
+            result.add(input[i]);
+        }
+        return result;
+    }
+
+    public static void quickSort(int[] array, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int pivot = partition(array, left, right);
+        quickSort(array, left, pivot - 1);
+        quickSort(array, pivot + 1, right);
+    }
+
+    public static int partition(int[] array, int left, int right) {
+        int pivot = array[right];
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array,i+1,right);
+        return i+1;
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+
+
+    /*public static ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
         ArrayList<Integer> result = new ArrayList<>();
         if (input == null || k <= 0 || k > input.length) {
             return result;
@@ -37,7 +78,7 @@ public class No40getLeastNumbers {
             result.add(minHeap.poll());
         }
         return result;
-    }
+    }*/
 
 
 }
