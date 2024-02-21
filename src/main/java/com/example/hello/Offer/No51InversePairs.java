0b32471b0ab7e13701f825bbf9abedfcc690dbd4
MeiHePing <sunhaolin1995@gmail.com>
2024-02-21 08:51:57 +0800

    inversePair

---
+++ src/main/java/com/example/hello/Offer/No51InversePairs.java
@@ -0,0 +1,15 @@
+package com.example.hello.Offer;
+
+public class No51InversePairs {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int InversePairs (int[] nums) {
+
+    }
+
+
+}
