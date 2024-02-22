18b752351b81d95d5091f7052a95157a1d6e48bd
haolinsun <2575453225@qq.com>
2024-02-22 17:40:39 +0800

    postorder dfs

--- src/main/java/com/example/hello/Offer/No55balanceTree.java
+++ src/main/java/com/example/hello/Offer/No55balanceTree.java
@@ -12,6 +12,24 @@ public class No55balanceTree {
     }
 
     public static boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    public static int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+
+    }
+
+    /*public static boolean isBalanced(TreeNode root) {
         if (root == null) {
             return true;
         } else {
@@ -26,5 +44,5 @@ public class No55balanceTree {
             return Math.max(height(root.left), height(root.right)) + 1;
         }
     }
-
+*/
 }
