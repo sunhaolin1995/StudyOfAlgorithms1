14438b2d55a64a1413faca630f4aac54274f0ac4
haolinsun <2575453225@qq.com>
2024-02-22 17:24:53 +0800

    dfs

---
+++ src/main/java/com/example/hello/Offer/No55balanceTree.java
@@ -0,0 +1,30 @@
+package com.example.hello.Offer;
+
+
+public class No55balanceTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(isBalanced(root));
+    }
+
+    public static boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    public static int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(height(root.left), height(root.right)) + 1;
+        }
+    }
+
+}
