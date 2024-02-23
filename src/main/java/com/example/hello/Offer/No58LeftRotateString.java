7cf84cebf01c80268664b23501ef409d738a06d6
haolinsun <2575453225@qq.com>
2024-02-23 10:30:15 +0800

    complete

--- src/main/java/com/example/hello/Offer/No58LeftRotateString.java
+++ src/main/java/com/example/hello/Offer/No58LeftRotateString.java
@@ -3,11 +3,20 @@ package com.example.hello.Offer;
 public class No58LeftRotateString {
 
     public static void main(String[] args) {
-
+        String str="";int n =3;
+        System.out.println(LeftRotateString(str, n));
     }
 
-    public String LeftRotateString (String str, int n) {
+    public static String LeftRotateString(String str, int n) {
+        if (str == null || str.length()<=1){
+            return str;
+        }
         // write code here
+        int offset = n % (str.length());
+
+        String sb1 = str.substring(offset, str.length() );
+        String sb2 = str.substring(0, offset);
+        return sb1 + sb2;
     }
 
 }
