6400b59f0c2355002c720df04aee8f94f0d3ee79
MeiHePing <sunhaolin1995@gmail.com>
2024-02-23 09:49:56 +0800

    to

---
+++ src/main/java/com/example/hello/Offer/No58LeftRotateString.java
@@ -0,0 +1,13 @@
+package com.example.hello.Offer;
+
+public class No58LeftRotateString {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String LeftRotateString (String str, int n) {
+        // write code here
+    }
+
+}
